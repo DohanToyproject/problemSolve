e9808d81b9ec75c2203705b7882a08b823d22bbe
suyug1 <133092778+suyug1@users.noreply.github.com>
2023-06-16 16:57:42 +0900

    Create 2675.java

---
+++ 0616/minsu/2675.java
@@ -0,0 +1,34 @@
+
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("S를 입력하세요: ");
+        String S = scanner.nextLine();
+
+        System.out.print(“R을 입력하세요: ");
+        int R = scanner.nextInt();
+
+        String P = repeatCharacters(S, R);
+
+        System.out.println("새 문자열 P: " + P);
+
+        scanner.close();
+    }
+
+   public static String repeatCharacters(String S, int R) {
+    String result = "";
+    for (int i = 0; i < S.length(); i++) {
+        char ch = S.charAt(i);
+        for (int j = 0; j < R; j++) {
+            result += ch;
+        }
+    }
+    return result;
+}
+
+}
